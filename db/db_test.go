c144f9b1baf9834fe2730faf0fc0ddd43138f034
shenhao <ilikehome@163.com>
2017-06-21 15:15:24 +0800

    finished base. go test ok.

--- db/db_test.go
+++ db/db_test.go
@@ -2,15 +2,42 @@ package db
 
 import (
 	"testing"
-	"fmt"
 )
 
-func TestingLogger(t *testing.T)  {
-	db := Open("d:\\shdb1\\1.txt")
+const (
+	dbPath = "c:\\shdb1\\1.txt"
+)
+
+func TestBaseRW(t *testing.T)  {
+	db := Open(dbPath)
 	defer db.Close()
 
-	db.Write([]byte("gggg24r"),[]byte("lllrrrlll4"))
-	fmt.Println(string(db.Read([]byte("gggg24r"))))
+	kStr := "gggg24r"
+	k := []byte(kStr)
+	vStr := "lllrrrlll4"
+	v := []byte(vStr)
+
+	db.Write(k, v)
+	buf,_ := db.Read(k)
+	if string(buf) != vStr{
+		t.Fatal("put get failed.")
+	}
+
+	vNewStr := "tttt"
+	vNew := []byte(vNewStr)
+
+	db.Write(k,vNew)
+	buf,_ = db.Read(k)
+	if string(buf) != vNewStr{
+		t.Fatal("update failed.")
+	}
+
+	kNotExist := []byte("cccccc")
+
+	buf,_ = db.Read(kNotExist)
+	if buf != nil{
+		t.Fatal("get not exist key failed.")
+	}
 }
 
 
