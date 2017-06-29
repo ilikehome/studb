88f50f587d4ce70da71c102cc2d0cc9ebef365d4
hao.shen <hao.shen@veritas.com>
2017-06-29 17:10:37 +0800

    interface add

--- db/cache/cache.go
+++ db/cache/cache.go
@@ -1,1 +1,21 @@
 package cache
+
+type Cache struct{
+
+}
+
+func CreateCache(size int) *Cache{
+	return nil
+}
+
+func(c *Cache)Put(k interface{}, v interface{}){
+
+}
+
+func(c *Cache)Del(k interface{}, v interface{}){
+
+}
+
+func(c *Cache)Get(k interface{}) ( v interface{}, e error){
+	return nil, nil
+}
