c40b59c75ff69187f55ae223e224c0d0b33be602
hao.shen <hao.shen@veritas.com>
2017-07-04 15:24:27 +0800

    start to cache

--- db/cache/cache.go
+++ db/cache/cache.go
@@ -1,21 +1,23 @@
 package cache
 
 type Cache struct{
-
+	m map[interface{}]interface{} //change to sync.Map in go1.9
 }
 
 func CreateCache(size int) *Cache{
-	return nil
+	c := new(Cache)
+	c.m = make(map[interface{}]interface{})
+	return c
 }
 
 func(c *Cache)Put(k interface{}, v interface{}){
-
+	c.m[k] = v
 }
 
-func(c *Cache)Del(k interface{}, v interface{}){
-
+func(c *Cache)Del(k interface{}){
+	delete(c.m, k)
 }
 
-func(c *Cache)Get(k interface{}) ( v interface{}, e error){
-	return nil, nil
+func(c *Cache)Get(k interface{}) interface{}{
+	return c.m[k]
 }
--- db/cache/cache_test.go
+++ db/cache/cache_test.go
@@ -1,30 +1,22 @@
-package index
+package cache
 
 import (
 	"testing"
-	"encoding/hex"
-	"os"
-	"github.com/ilikehome/studb/db/constant"
 )
 
-const (
-	indexInDiskPath = "/1.inx.txt"
-)
 
 func TestBase(t *testing.T) {
-	f,_ := os.OpenFile(indexInDiskPath, os.O_RDWR|os.O_CREATE, 0666);
-	iid := openDiskIndex(f)
-	defer iid.close()
+	c := CreateCache(10)
+	c.Put([3]byte{1,2,3}, "i")
+	v := c.Get([3]byte{1,2,3})
+	if v != "i" {
+		t.Fatal("Get fail.")
+	}
 
-	iid.append(1, constant.OP_PUT, []byte{'a', 'b', 'c'}, 64)
-	iid.append(2, constant.OP_PUT, []byte{'a', 'b', 'w'},164)
-	iid.append(3, constant.OP_PUT, []byte{'a', 'b', 'w'},264)
-	iid.append(4, constant.OP_PUT, []byte{'a', 'b', 'w'},364)
-	iid.append(5, constant.OP_PUT, []byte{'a', 'y', 'c'},464)
-
-	m := iid.readToMem()
-	if m[hex.EncodeToString([]byte{'a','y','c'})] != 464{
-		t.Fatal("Load index from disk error.")
+	c.Del([3]byte{1,2,3})
+	v = c.Get([3]byte{1,2,3})
+	if v != nil {
+		t.Fatal("Del fail.")
 	}
 }
 
